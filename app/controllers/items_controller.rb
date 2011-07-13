9c2d95df08a4e3df909289cd8aea356847d357ca
Roberto Soares <roberto.tech@gmail.com>
2011-07-13 17:29:00 -0300

    action show (stub)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,10 @@ class ItemsController < ApplicationController
     @reserved_items = Item.where("giver_id IS NOT NULL").all
   end
 
+  def show
+    redirect_to items_url
+  end
+
   def update
     @item = Item.find params[:id]
     if @item.giver.present?

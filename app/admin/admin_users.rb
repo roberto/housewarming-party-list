9102b6e12ca39ff04e2e2da2e4c9f6ccd4bf2477
Roberto Soares <roberto.tech@gmail.com>
2011-07-14 10:13:51 -0300

    added admin_users admin

---
+++ app/admin/admin_users.rb
@@ -0,0 +1,16 @@
+ActiveAdmin.register AdminUser do
+
+  index do
+    column :email
+    default_actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :email
+      f.input :password
+    end
+    f.buttons
+  end
+
+end
